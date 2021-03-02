5bc9159b9a8538c770607b25e09f1394f74e9122
Breno Silva <brenophp@gmail.com>
2021-03-01 23:42:49 -0300

    feat:: adds support to steal only matched labels

--- src/steal_labels.py
+++ src/steal_labels.py
@@ -2,6 +2,8 @@ import os
 import torch
 import numpy as np
 
+from tqdm import tqdm
+
 from torchvision.utils import save_image
 from torchvision.datasets import ImageFolder
 from torchvision.transforms import Resize, Compose, Normalize, ToTensor
@@ -18,11 +20,11 @@ model = model.to(DEVICE)
 
 norm = Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
 
-dataset_folder = 'data/copycat_generated_dataset'
-stolen_labels_folder = 'data/copycat_generated_dataset_stolen_labels'
+dataset_folder = 'data/dataset_gan116'
+stolen_labels_folder = 'data/dataset_gan116_sl'
 
 transform = Compose([
-    Resize((32, 32)),
+    #Resize((32, 32)),
     ToTensor(),
 ])
 
@@ -31,25 +33,28 @@ dataset = ImageFolder(root=dataset_folder, transform=transform)
 if not os.path.exists(stolen_labels_folder):
     os.mkdir(stolen_labels_folder)
 
-from tqdm import tqdm
-
 real_labels = np.array([])
 pred_labels = np.array([])
+
+true_labels = False
 
 print('Generating labels from target...')
 with torch.no_grad():
     model.eval()
 
-    for images, labels in tqdm(dataset):
-        real_labels = np.append(real_labels, labels)
-        images = images.to(DEVICE)
+    for image, label in tqdm(dataset):
+        real_labels = np.append(real_labels, label)
+        image = image.to(DEVICE)
 
         # simulando o processamento da api, pois eu n conheco a norm e preciso salvar a imagem original
-        images_norm = norm(images.view(3, 32, 32)).view(1, 3, 32, 32)
+        images_norm = norm(image.view(3, 32, 32)).view(1, 3, 32, 32)
 
         outputs = model(images_norm)
         _, predicted = torch.max(outputs.data, 1)
         predicted = predicted.cpu().item()
+
+        if (true_labels) and (label != predicted):
+            continue
 
         new_idx = len(pred_labels[pred_labels == predicted])
         pred_labels = np.append(pred_labels, predicted)
@@ -59,7 +64,7 @@ with torch.no_grad():
         os.makedirs(label_folder, exist_ok=True)
 
         save_image(
-            images,
+            image,
             fp=os.path.join(label_folder, f'{new_idx}.png')
         )
 
