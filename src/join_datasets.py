0ca39768bb47e3d1d937101cad7c620c8b8e9c24
Breno Silva <brenophp@gmail.com>
2021-03-01 23:40:27 -0300

    feat:: adds join datasets script

---
+++ src/join_datasets.py
@@ -0,0 +1,77 @@
+import os
+import torch
+import numpy as np
+
+from tqdm import tqdm
+
+from torch.utils.data import DataLoader
+
+from torchvision.utils import save_image
+from torchvision.datasets import ImageFolder
+from torchvision.transforms import Resize, Compose, ToTensor
+
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+np.set_printoptions(suppress=True)
+
+dataset1_folder = 'data/dataset_ii'
+dataset2_folder = 'data/dataset_gan118_sl'
+
+new_folder = "data/dataset_ii+dataset_gan118_sl"
+
+dataset1 = ImageFolder(
+    root=dataset1_folder, 
+    transform=Compose([
+        # Resize((32, 32)),
+        ToTensor(),
+    ])
+)
+loader1 = DataLoader(dataset1, shuffle=True, batch_size=1)
+
+dataset2 = ImageFolder(
+    root=dataset2_folder, 
+    transform=Compose([
+        # Resize((32, 32)),
+        ToTensor(),
+    ])
+)
+loader2 = DataLoader(dataset2, shuffle=True, batch_size=1)
+
+os.makedirs(new_folder, exist_ok=True)
+
+labels = {k:0 for k in np.arange(10)}
+
+
+for label in labels.keys():
+    label_folder = os.path.join(new_folder, str(label))
+    os.makedirs(label_folder, exist_ok=True)
+
+print('Joining datasets...')
+for image, label in tqdm(loader1):
+    label = label.item()
+    label_folder = os.path.join(new_folder, str(label))
+
+    image = image.to(DEVICE)
+
+    i = labels[label]
+    labels[label] += 1
+
+    save_image(
+        image,
+        fp=os.path.join(label_folder, f'{i}.png')
+    )
+
+
+for image, label in tqdm(loader2):
+    label = label.item()
+    label_folder = os.path.join(new_folder, str(label))
+
+    image = image.to(DEVICE)
+
+    i = labels[label]
+    labels[label] += 1
+
+    save_image(
+        image,
+        fp=os.path.join(label_folder, f'{i}.png')
+    )
