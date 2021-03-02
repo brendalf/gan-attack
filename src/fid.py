c1148a2b30466842f094afa497784a38f89c5e41
Breno Silva <brenophp@gmail.com>
2021-03-01 23:41:42 -0300

    feat:: adds stats file support to fid

--- src/fid.py
+++ src/fid.py
@@ -9,6 +9,16 @@ from torch.nn import functional as F
 
 from torchvision.models.inception import inception_v3
 
+def read_stats_file(filepath):
+    """read mu, sigma from .npz"""
+    if filepath.endswith('.npz'):
+        f = np.load(filepath)
+        m, s = f['mu'][:], f['sigma'][:]
+        f.close()
+    else:
+        raise Exception('ERROR! pls pass in correct npz file %s' % filepath)
+    return m, s
+
 class FID:
     def __init__(self):
         self.__image_size = (299,299,3)
@@ -36,38 +46,28 @@ class FID:
         return pool3_ft, preds
 
     def __calc_stats(self, pool3_ft):
+        # calculate mean and covariance statistics
         mu = np.mean(pool3_ft, axis=0)
         sigma = np.cov(pool3_ft, rowvar=False)
         return mu, sigma
 
-    def calculate_fid(self, images1, images2, batch_size=16):
-        assert images1.shape[0] == images2.shape[0]
-
+    def calculate_statistics(self, images, batch_size=16):
         # calculate activations
-        n_img = images1.shape[0]
+        n_img = images.shape[0]
 
-        pool3_ft_1, preds_1 = np.zeros((n_img, 2048)), np.zeros((n_img, 1000))
-        pool3_ft_2, preds_2 = np.zeros((n_img, 2048)), np.zeros((n_img, 1000))
+        pool3_ft, preds = np.zeros((n_img, 2048)), np.zeros((n_img, 1000))
 
         for i in range(np.int32(np.ceil(1.0 * n_img / batch_size))):
             batch_size_i = min((i+1) * batch_size, n_img) - i * batch_size
 
-            batchv_1 = Variable(images1[i * batch_size:i * batch_size + batch_size_i, ...].type(self.dtype))
-            pool3_ft_1[i * batch_size:i * batch_size + batch_size_i], preds_1[i * batch_size:i * batch_size + batch_size_i] = self.__forward(batchv_1)
+            batchv = Variable(images[i * batch_size:i * batch_size + batch_size_i, ...].type(self.dtype))
+            pool3_ft[i * batch_size:i * batch_size + batch_size_i], preds[i * batch_size:i * batch_size + batch_size_i] = self.__forward(batchv)
 
-            batchv_2 = Variable(images2[i * batch_size:i * batch_size + batch_size_i, ...].type(self.dtype))
-            pool3_ft_2[i * batch_size:i * batch_size + batch_size_i], preds_2[i * batch_size:i * batch_size + batch_size_i] = self.__forward(batchv_2)
+        mu, sigma = self.__calc_stats(pool3_ft)
 
+        return mu, sigma
 
-        # calculate mean and covariance statistics
-        mu1, sigma1 = self.__calc_stats(pool3_ft_1)
-        mu2, sigma2 = self.__calc_stats(pool3_ft_2)
-
-        # calculate score
-        fid = self.__calculate_frechet_distance(mu1, sigma1, mu2, sigma2)
-        return fid
-
-    def __calculate_frechet_distance(self, mu1, sigma1, mu2, sigma2, eps=1e-6):
+    def calculate_fid(self, mu1, sigma1, mu2, sigma2, eps=1e-6):
         mu1 = np.atleast_1d(mu1)
         mu2 = np.atleast_1d(mu2)
 
@@ -79,7 +79,7 @@ class FID:
         assert sigma1.shape == sigma2.shape, \
             'Training and test covariances have different dimensions %s, %s' % (sigma1.shape, sigma2.shape)
         diff = mu1 - mu2
-        # Product might be almost singular
+
         covmean, _ = linalg.sqrtm(sigma1.dot(sigma2), disp=False)
         if not np.isfinite(covmean).all():
             msg = ('fid calculation produces singular product; '
@@ -87,11 +87,8 @@ class FID:
             print(msg)
             offset = np.eye(sigma1.shape[0]) * eps
             covmean = linalg.sqrtm((sigma1 + offset).dot(sigma2 + offset))
-        # Numerical error might give slight imaginary component
+
         if np.iscomplexobj(covmean):
-            #if not np.allclose(np.diagonal(covmean).imag, 0, atol=1e-3):
-            #    m = np.max(np.abs(covmean.imag))
-            #    raise ValueError('Imaginary component {}'.format(m))
             covmean = covmean.real
         tr_covmean = np.trace(covmean)
         return diff.dot(diff) + np.trace(sigma1) + np.trace(sigma2) - 2 * tr_covmean
