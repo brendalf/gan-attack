c582ad2786fa1a4e9bdfa8ca6d25786773ba523f
Breno Silva <brenophp@gmail.com>
2021-04-21 20:21:51 -0300

    updating vgg to accept different outputs

---
+++ src/models/dcgan_32.py
@@ -0,0 +1,156 @@
+import torch
+import torch.nn as nn
+
+# Number of channels in the training images. For color images this is 3
+nc = 3
+
+# Size of feature maps in generator
+ngf = 128
+ndf = 64
+
+class Generator(nn.Module):
+    def __init__(self, latent_dim, n_class):
+        super(Generator, self).__init__()
+        self.main = nn.Sequential(
+            # input is Z, going into a convolution
+            nn.ConvTranspose2d(
+                in_channels=latent_dim + n_class, 
+                out_channels=ngf*8,
+                kernel_size=4,
+                stride=1,
+                padding=0,
+                bias=False
+            ),
+            nn.BatchNorm2d(num_features=ngf*8),
+            nn.ReLU(inplace=True),
+
+            # state size => (ngf*8) x 4 x 4
+            nn.ConvTranspose2d(
+                in_channels=ngf*8, 
+                out_channels=ngf*4,
+                kernel_size=4,
+                stride=2,
+                padding=1,
+                bias=False
+            ),
+            nn.BatchNorm2d(num_features=ngf*4),
+            nn.ReLU(inplace=True),
+
+            # state size => (ngf*4) x 8 x 8
+            nn.ConvTranspose2d(
+                in_channels=ngf*4, 
+                out_channels=ngf*2,
+                kernel_size=4,
+                stride=2,
+                padding=1,
+                bias=False
+            ),
+            nn.BatchNorm2d(num_features=ngf*2),
+            nn.ReLU(inplace=True),
+
+            # state size => (ngf*2) x 16 x 16
+            nn.ConvTranspose2d(
+                in_channels=ngf*2, 
+                out_channels=nc,
+                kernel_size=4,
+                stride=2,
+                padding=1,
+                bias=False
+            ),
+            nn.Tanh(),
+
+            # state size => (nc) x 32 x 32 
+        )
+
+    def forward(self, inputs, condition):
+        # concatenate noise and condition
+        concatenated_input = torch.cat(
+            (inputs, condition),
+            dim=1
+        )
+
+        # reshape the latent vector into a feature map
+        concatenated_input = concatenated_input.unsqueeze(2).unsqueeze(3)
+
+        return self.main(concatenated_input)
+
+class Discriminator(nn.Module):
+    def __init__(self, n_class):
+        super(Discriminator, self).__init__()
+        self.main = nn.Sequential(
+            # state size => (ndf) x 32 x 32 
+            nn.Conv2d(
+                in_channels=nc, 
+                out_channels=ndf,
+                kernel_size=4,
+                stride=2,
+                padding=1,
+                bias=False
+            ),
+            nn.LeakyReLU(
+                negative_slope=0.2,
+                inplace=True
+            ),
+
+            # state size => (ndf) x 16 x 16 
+            nn.Conv2d(
+                in_channels=ndf, 
+                out_channels=ndf*2,
+                kernel_size=4,
+                stride=2,
+                padding=1,
+                bias=False
+            ),
+            nn.BatchNorm2d(num_features=ndf*2),
+            nn.LeakyReLU(
+                negative_slope=0.2,
+                inplace=True
+            ),
+
+            # state size => (ndf*2) x 8 x 8 
+            nn.Conv2d(
+                in_channels=ndf*2, 
+                out_channels=ndf*4,
+                kernel_size=4,
+                stride=2,
+                padding=1,
+                bias=False
+            ),
+            nn.BatchNorm2d(num_features=ndf*4),
+            nn.LeakyReLU(
+                negative_slope=0.2,
+                inplace=True
+            ),
+        )
+
+        # categorical classifier
+        self.clf = nn.Sequential(
+            nn.Linear(
+                in_features=ndf*4*4*4,
+                out_features=n_class,
+                bias=True
+            ),
+            nn.Softmax(dim=1)
+        )
+        
+        # real / fake classifier
+        self.police = nn.Sequential(
+            # state size => (ndf*8) x 4 x 4 
+            nn.Conv2d(
+                # in_channels=ndf*8,
+                in_channels=ndf*4,
+                out_channels=1,
+                kernel_size=4,
+                stride=1,
+                padding=0,
+                bias=False
+            ),
+            nn.Sigmoid()
+        )
+
+    def forward(self, inputs):
+        features = self.main(inputs)
+        is_true = self.police(features).view(-1, 1)
+        class_predicted = self.clf(features.view(features.shape[0], -1))
+
+        return is_true, class_predicted
--- src/oraculo_train.py
+++ src/oraculo_train.py
@@ -1,4 +1,3 @@
-'''Train CIFAR10 with PyTorch.'''
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -31,29 +30,37 @@ transform_train = transforms.Compose([
     transforms.RandomCrop(32, padding=4),
     transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    # transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
 transform_test = transforms.Compose([
     transforms.Resize((32, 32)),
     transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    # transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
-trainset = torchvision.datasets.CIFAR10(
-    root='./data', train=True, download=True, transform=transform_train)
+# trainset = torchvision.datasets.CIFAR10(
+    # root='./data', train=True, download=True, transform=transform_train)
+trainset = torchvision.datasets.ImageFolder(
+    root="./data/GOC/OD", transform=transform_train
+)
 trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=128, shuffle=True, num_workers=2)
+    trainset, batch_size=128, shuffle=True, num_workers=2
+)
 
-testset = torchvision.datasets.CIFAR10(
-    root='./data', train=False, download=True, transform=transform_test)
+# testset = torchvision.datasets.CIFAR10(
+    # root='./data', train=False, download=True, transform=transform_test)
+testset = torchvision.datasets.ImageFolder(
+    root="./data/GOC/TD", transform=transform_test
+)
 testloader = torch.utils.data.DataLoader(
-    testset, batch_size=100, shuffle=False, num_workers=2)
+    testset, batch_size=100, shuffle=False, num_workers=2
+)
 
 # Model
 print('==> Building model...')
 #net = VGG('VGG19')
-net = VGG16(pretrained=True)
+net = VGG16(out_features=9, pretrained=True)
 net = net.to(device)
 
 if device == 'cuda':
